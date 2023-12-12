6964c7882dd440c909aed151a94ad7dc1d40e0dc
anjar <anjar.bra@gmail.com>
2023-12-12 10:32:24 +0100

    Set up nested route for lesson page

--- pages/course.vue
+++ pages/course.vue
@@ -2,18 +2,19 @@
   <div
     class="flex flex-col items-center p-6 pt-10 w-full h-full min-h-screen max-w-[1200px] mx-auto"
   >
-    <div class="porse mb-10">
+    <div class="prose mb-10">
       <h1>
-        <span class="font-medium"> Course: </span> <span class="font-bold">Nuxt router</span>
+        <span class="font-medium"> Course: </span>
+        <span class="font-bold">Nuxt router</span>
       </h1>
     </div>
 
-    <div class="flex flex-grow gap-6 porse h-ful w-full">
-      <div class="p-6 rounded bg-slate-200 w-[25%]">
+    <div class="flex flex-grow gap-6 h-full w-full">
+      <div class="prose p-6 rounded bg-slate-200 w-[25%]">
         <h2>Chapters</h2>
       </div>
-      <div class="p-6 rounded bg-slate-200 w-full">
-        <span>Lessons</span>
+      <div class="prose p-6 rounded bg-slate-200 w-full">
+        <NuxtPage />
       </div>
     </div>
   </div>
---
+++ pages/course/lesson.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Lesson</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi veritatis
+    voluptatum nostrum. Pariatur eaque tenetur asperiores aliquam odit repellat
+    deserunt perspiciatis temporibus sunt molestiae? Animi totam doloremque
+    saepe sed odit?
+  </p>
+</template>
