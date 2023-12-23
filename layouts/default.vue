6db28bcc025cdb8dc370b7d6e069a53307e65278
anjar <anjar.bra@gmail.com>
2023-12-23 17:01:01 +0100

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex flex-col items-center p-6 pt-10 w-full h-full min-h-screen max-w-[1200px] mx-auto"
+  >
+    <slot />
+  </div>
+</template>
--- pages/course.vue
+++ pages/course.vue
@@ -1,39 +1,35 @@
 <template>
-  <div
-    class="flex flex-col items-center p-6 pt-10 w-full h-full min-h-screen max-w-[1200px] mx-auto"
-  >
-    <div class="prose mb-10">
-      <h1>
-        <span class="font-medium"> Course: </span>
-        <span class="font-bold">Nuxt router</span>
-      </h1>
-    </div>
+  <div class="prose mb-10">
+    <h1>
+      <span class="font-medium"> Course: </span>
+      <span class="font-bold">Nuxt router</span>
+    </h1>
+  </div>
 
-    <div class="flex flex-grow gap-6 h-full w-full">
-      <div class="prose p-6 rounded bg-white w-[25%]">
-        <h2>Chapters</h2>
-        <ul class="list-none pl-0">
-          <li v-for="(chapter, index) in chapters" :key="chapter.slug">
-            <h3>{{ chapter.title }}</h3>
-            <ol class="list-decimal">
-              <li v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
-                <NuxtLink
-                  class="no-underline"
-                  :class="{
-                    'text-blue-400': lesson.path === $route.fullPath,
-                    'text-gray-400': lesson.path !== $route.fullPath,
-                  }"
-                  :to="lesson.path"
-                  >{{ lesson.title }}</NuxtLink
-                >
-              </li>
-            </ol>
-          </li>
-        </ul>
-      </div>
-      <div class="prose p-6 rounded bg-white w-full max-w-full">
-        <NuxtPage />
-      </div>
+  <div class="flex flex-grow gap-6 h-full w-full">
+    <div class="prose p-6 rounded bg-white w-[25%]">
+      <h2>Chapters</h2>
+      <ul class="list-none pl-0">
+        <li v-for="(chapter, index) in chapters" :key="chapter.slug">
+          <h3>{{ chapter.title }}</h3>
+          <ol class="list-decimal">
+            <li v-for="(lesson, index) in chapter.lessons" :key="lesson.slug">
+              <NuxtLink
+                class="no-underline"
+                :class="{
+                  'text-blue-400': lesson.path === $route.fullPath,
+                  'text-gray-400': lesson.path !== $route.fullPath,
+                }"
+                :to="lesson.path"
+                >{{ lesson.title }}</NuxtLink
+              >
+            </li>
+          </ol>
+        </li>
+      </ul>
+    </div>
+    <div class="prose p-6 rounded bg-white w-full max-w-full">
+      <NuxtPage />
     </div>
   </div>
 </template>
