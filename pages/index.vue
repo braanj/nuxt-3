a3cd675a5b5cf1b4f1a3c4071af9e01fa667801d
Brahim Anjjar <brahim@devforyou.fr>
2023-12-28 20:36:40 +0100

    Add login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,11 @@
-<template>Hello, world!</template>
+<template>
+  <NuxtLink
+    class="py-2 px-4 rounded bg-blue-400 font-bold text-white"
+    :to="lesson.path"
+    >Go to first lesson</NuxtLink
+  >
+</template>
+
+<script setup>
+const lesson = useFirstLesson();
+</script>
