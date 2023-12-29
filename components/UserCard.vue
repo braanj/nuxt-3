aa92c8151ce21559ae8668c50ce7b0cda7d8c2e2
Brahim Anjjar <brahim@devforyou.fr>
2023-12-29 18:59:19 +0100

    Set up supabase and login with github

---
+++ components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex gap-3 items-center py-3 px-4 bg-white rounded">
+    <img
+      :src="user.avatar"
+      class="w-10 h-10 rounded-full"
+      alt="github user profile image"
+    />
+    <div class="flex flex-col">
+      <span class="font-bold">{{ user.name }}</span>
+      <button class="text-start">Log out</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const USER = useSupabaseUser();
+
+const user = computed(() => {
+  return {
+    name: USER.value.user_metadata.name,
+    avatar: USER.value.user_metadata.avatar_url,
+  };
+});
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-col">
     <h1>Log in to {{ title }} course!</h1>
-    <button class="px-6 py-4 bg-blue-400 rounded font-bold text-white mt-4">
+    <button
+      @click="login"
+      class="px-6 py-4 bg-blue-400 rounded font-bold text-white mt-4"
+    >
       Login with github
     </button>
   </div>
@@ -9,4 +12,15 @@
 
 <script setup>
 const { title } = useCourse();
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+
+  if (error) {
+    console.log(error);
+  }
+};
 </script>
