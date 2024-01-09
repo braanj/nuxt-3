0514d802b1db42cdd81b81793bcfd97762b37c8d
Brahim Anjjar <brahim@devforyou.fr>
2024-01-09 18:31:11 +0100

    Type api endpoints

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,31 +1,9 @@
+import type { Chapter, Lesson, Course, LessonWithPAth } from "~/types/course";
 import course from "./courseData";
 
-type Lesson = {
-  title: string;
-  slug: string;
-  number: number;
-  downloadUrl: string;
-  videoId: number;
-  text: string;
-  sourceUrl?: string;
-  path: string;
-};
-
-type Chapter = {
-  title: string;
-  slug: string;
-  number: number;
-  lessons: Lesson[];
-};
-
-type Course = {
-  title: string;
-  chapters: Chapter[];
-};
-
 export const useCourse = (): Course => {
-  const chapters: Chapter[] = course.chapters.map((chapter) => {
-    const lessons: Lesson[] = chapter.lessons.map((lesson) => ({
+  const chapters: Chapter[] = course.chapters.map((chapter: Chapter) => {
+    const lessons: LessonWithPAth[] = chapter.lessons.map((lesson: Lesson) => ({
       ...lesson,
       path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
     }));
